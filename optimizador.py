48b9ee464563befeedbe2e54871ef34750b17c4d
SantRR <tiago.romo05@gmail.com>
2025-05-06 20:02:49 -0600

    fix(optimizador.py)
    Cambio en cómo se obtiene el gradiente
    Cambio en gitignore
    Arreglo en gradientes.py
    Adaptación de main.py para los nuevos cambios

--- optimizador.py
+++ optimizador.py
@@ -2,22 +2,9 @@ import autograd.numpy as np
 from autograd import grad
 
 
-def grad_x(u):
-    return u[1:, :] - u[:-1, :]
+def func_objetivo(f, u, param_lambda):
+    grad_u = np.gradient(u)
+    grad_u = np.array(grad_u)  # Convierte a arreglo para hace operaciones vectoriales
+    return 0.5 * np.sum((u - f)**2) + 0.5 * param_lambda * np.sum(grad_u**2)
 
-
-def grad_y(u):
-    return u[:, 1:] - u[:, :-1]
-
-
-def grad_norma_cuadrada(u):
-    gx = grad_x(u)
-    gy = grad_y(u)
-    return np.sum(gx**2) + np.sum(gy**2)
-
-
-def func_loss(u, f, lambdaaaaa):
-    return 0.5 * np.sum((u - f) ** 2) + 0.5 * lambdaaaaa * grad_norma_cuadrada(u)
-
-
-grad_loss = grad(func_loss, 0)
+grad_func_objetivo = grad(func_objetivo)
